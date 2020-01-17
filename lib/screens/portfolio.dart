327a5c1614ef2ac24412d0256db225eb231d4dcd
athulantonynp <athulantonynp@gmail.com>
2020-01-17 22:08:48 +0530

    add portfolio page list

--- lib/screens/portfolio.dart
+++ lib/screens/portfolio.dart
@@ -1,26 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:monster/api/apimanager.dart';
+import 'package:monster/models/shot.dart';
 import 'package:monster/utils/ColorUtils.dart';
 import 'package:monster/utils/utils.dart';
 
-class Portfolio extends StatefulWidget{
+class Portfolio extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     return PortfolioState();
   }
-
 }
 
-class PortfolioState extends State<Portfolio>{
+class PortfolioState extends State<Portfolio> {
+  var ports = ApiManager().getCurrentShots();
+
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       backgroundColor: ColorUtils.fromHex("#212121"),
       body: new Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Expanded(
-            child:getBodyContent(context) ,
+            child: getBodyContent(context),
             flex: 13,
           ),
           Expanded(
@@ -32,19 +34,63 @@ class PortfolioState extends State<Portfolio>{
     );
   }
 
-
-  Widget getBodyContent(BuildContext context){
+  Widget getBodyContent(BuildContext context) {
     return Container(
-              color: ColorUtils.fromHex("#121212"),
-              child: Stack(
-                children: <Widget>[
-                  
-                ],
-              ),
+      color: ColorUtils.fromHex("#121212"),
+      child: FutureBuilder(
+        future: ports,
+        builder: (BuildContext context, AsyncSnapshot<List<Shot>> snapshot) {
+          if (snapshot.hasData) {
+            return GridView.builder(
+              shrinkWrap: true,
+              gridDelegate: new SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 4),
+              itemCount: snapshot.data.length,
+              itemBuilder: (context, index) {
+                return Column(
+                  children: <Widget>[
+                    Padding(
+                      child: getShotCard(snapshot.data[index]),
+                      padding: EdgeInsets.fromLTRB(16, 0, 16, 0),
+                    )
+                  ],
+                );
+              },
+            );
+          } else {
+            return SizedBox(
+                height: 300.0,
+                width: 300.0,
+                child: CircularProgressIndicator(
+                    valueColor: AlwaysStoppedAnimation(Colors.white),
+                    strokeWidth: 5.0));
+          }
+        },
+      ),
     );
   }
 
-    Widget getFooter(BuildContext context) {
+  Card getShotCard(Shot shot) {
+    return Card(
+      semanticContainer: true,
+      color: Colors.black,
+      clipBehavior: Clip.antiAliasWithSaveLayer,
+      child: Stack(
+        children: <Widget>[
+          Image.network(
+            shot.images.normal,
+            fit: BoxFit.fill,
+          ),
+        ],
+      ),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(16.0),
+      ),
+      elevation: 3,
+    );
+  }
+
+  Widget getFooter(BuildContext context) {
     return Container(
       color: Colors.black,
       child: Column(
@@ -58,12 +104,14 @@ class PortfolioState extends State<Portfolio>{
             alignment: Alignment.center,
             children: <Widget>[
               Row(
-                
                 crossAxisAlignment: CrossAxisAlignment.center,
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
-                  Text( "© 2020 Inspired Monster", style: TextStyle(color: ColorUtils.fromHex("#757575")),
-                textAlign: TextAlign.center,)
+                  Text(
+                    "© 2020 Inspired Monster",
+                    style: TextStyle(color: ColorUtils.fromHex("#757575")),
+                    textAlign: TextAlign.center,
+                  )
                 ],
               ),
               Align(
@@ -71,7 +119,8 @@ class PortfolioState extends State<Portfolio>{
                 child: Padding(
                   padding: EdgeInsets.fromLTRB(0, 0, 16, 0),
                   child: IconButton(
-                    icon: Image.asset(Utils.getImageForWeb("ic_mode_light.png")),
+                    icon:
+                        Image.asset(Utils.getImageForWeb("ic_mode_light.png")),
                     onPressed: () {},
                   ),
                 ),
@@ -82,5 +131,4 @@ class PortfolioState extends State<Portfolio>{
       ),
     );
   }
-
 }
