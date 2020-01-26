dadb35ceb51bb301abc6eee84c29d074e606fad4
athulantonynp <athulantonynp@gmail.com>
2020-01-26 19:47:21 +0530

    add abotu me content

--- lib/screens/aboutme.dart
+++ lib/screens/aboutme.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:monster/widgets/bottombar.dart';
 
 class AboutMe extends StatefulWidget{
   @override
@@ -11,7 +12,22 @@ class AboutMe extends StatefulWidget{
 class AboutMeState extends State<AboutMe>{
   @override
   Widget build(BuildContext context) {
-    return Text("About me");
+   return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: new Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Expanded(
+            child: Container(),
+            flex: 13,
+          ),
+          Expanded(
+            child: BottomBar(),
+            flex: 1,
+          ),
+        ],
+      ),
+    );
   }
 
 }
