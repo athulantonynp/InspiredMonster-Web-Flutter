67172f7418c8a2c8f5cb5dfc5479a4f30373ba51
athulantonynp <athulantonynp@gmail.com>
2020-02-09 19:33:57 +0530

    convert the shot detail to carousel ?

--- lib/screens/shotdetail.dart
+++ lib/screens/shotdetail.dart
@@ -4,25 +4,23 @@ import 'package:monster/models/shot.dart';
 import 'package:monster/utils/utils.dart';
 
 class ShotDetail extends StatefulWidget {
-
   Shot shot;
   List<Shot> shots;
   int index;
-  ShotDetail({this.shot,this.shots,this.index}) : super();
+
+  ShotDetail({this.shot, this.shots, this.index}) : super();
 
   @override
   _ShotDetailState createState() => _ShotDetailState();
 }
 
 class _ShotDetailState extends State<ShotDetail> {
-
   @override
   Widget build(BuildContext context) {
-
-    var slider=buildCarousel(widget,context);
+    var slider = buildCarousel(widget, context);
 
     return Scaffold(
-      body:  Column(
+      body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Expanded(
@@ -38,41 +36,80 @@ class _ShotDetailState extends State<ShotDetail> {
     );
   }
 
-   CarouselSlider buildCarousel(ShotDetail widget,BuildContext context){
+  CarouselSlider buildCarousel(ShotDetail widget, BuildContext context) {
     var imageWidth = getShotImageWidthForPreview(context);
+    var residueSpace = (MediaQuery.of(context).size.width - imageWidth) / 4;
     return CarouselSlider.builder(
         itemCount: widget.shots.length,
-        aspectRatio: MediaQuery.of(context).size.width/MediaQuery.of(context).size.height,
-      enableInfiniteScroll: true,
-      enlargeCenterPage: true,
-      scrollDirection: Axis.horizontal,
-      initialPage: widget.index,
-      itemBuilder: (BuildContext context, int itemIndex) =>
-
-          Container(
-            width: imageWidth,
-              height:(imageWidth * 3) / 4,
-              child: Stack(
-                alignment:Alignment.center,
+        aspectRatio: MediaQuery.of(context).size.width / MediaQuery.of(context).size.height,
+        enableInfiniteScroll: true,
+        enlargeCenterPage:false,
+        scrollDirection: Axis.horizontal,
+        initialPage: widget.index,
+        itemBuilder: (BuildContext context, int itemIndex) => Container(
+              margin: EdgeInsets.fromLTRB(residueSpace, 0, residueSpace, 0),
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              child: ListView(
+                shrinkWrap: true,
+                padding: EdgeInsets.all(24),
                 children: <Widget>[
-                  Center(
-                      child: CircularProgressIndicator(
-                      valueColor: new AlwaysStoppedAnimation<Color>(Theme.of(context).indicatorColor)
-                  )),
-                  Image.network(
-
-                    widget.shots[itemIndex].images.two_x,
-                    fit: BoxFit.fill,
-                    width: imageWidth,
-                    height: (imageWidth * 3) / 4,
+                  Align(
+                    alignment: Alignment.center,
+                    child: Container(
+                      width: imageWidth,
+                      height: (imageWidth * 3) / 4,
+                      child:Stack(
+                        children: <Widget>[
+                          Center(child: CircularProgressIndicator(
+                              valueColor: new AlwaysStoppedAnimation<Color>(Theme.of(context).indicatorColor)
+                          )),
+                          Image.network(
+                            widget.shots[itemIndex].images.two_x,
+                            fit: BoxFit.fill,
+                            width: imageWidth,
+                            height: (imageWidth*3)/4,
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Padding(
+                            padding: EdgeInsets.fromLTRB(0, 24, 0, 8),
+                            child: Text(
+                              widget.shots[itemIndex].title,
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  color: Theme.of(context).indicatorColor),
+                              textAlign: TextAlign.left,
+                              softWrap: false,
+                            ),
+                          ),
+                        ),
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            removeAllHtmlTags(widget.shots[itemIndex].description),
+                            style: TextStyle(
+                                fontSize: 14,
+                                color: Theme.of(context).indicatorColor),
+                          ),
+                        )
+                      ],
+                    ),
                   )
                 ],
               ),
-            )
-    );
+            ));
   }
-
-
 
   String removeAllHtmlTags(String htmlText) {
     RegExp exp = RegExp(r"<[^>]*>", multiLine: true, caseSensitive: true);
@@ -122,23 +159,31 @@ class _ShotDetailState extends State<ShotDetail> {
         crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
           InkWell(
-            child: RotatedBox(quarterTurns: 2,child: Image.asset(Utils.getImageForWeb("ic_forward.png")),),
-            onTap: (){
+            child: RotatedBox(
+              quarterTurns: 2,
+              child: Image.asset(Utils.getImageForWeb("ic_forward.png")),
+            ),
+            onTap: () {
               setState(() {
-                slider.previousPage(duration: Duration(milliseconds: 300), curve: Curves.linear);
-
+                slider.previousPage(
+                    duration: Duration(milliseconds: 300),
+                    curve: Curves.linear);
               });
             },
           ),
-         InkWell(
-           child:  Image.asset(Utils.getImageForWeb("ic_close.png")),
-           onTap: (){Navigator.of(context).pop();},
-         ),
+          InkWell(
+            child: Image.asset(Utils.getImageForWeb("ic_close.png")),
+            onTap: () {
+              Navigator.of(context).pop();
+            },
+          ),
           InkWell(
             child: Image.asset(Utils.getImageForWeb("ic_forward.png")),
-            onTap: (){
+            onTap: () {
               setState(() {
-                slider.nextPage(duration: Duration(milliseconds: 300), curve: Curves.linear);
+                slider.nextPage(
+                    duration: Duration(milliseconds: 300),
+                    curve: Curves.linear);
               });
             },
           )
@@ -146,67 +191,4 @@ class _ShotDetailState extends State<ShotDetail> {
       ),
     );
   }
-
-
-/*  Center(
-  child: Container(
-  width: imageWidth,
-  child: ListView(
-  shrinkWrap: true,
-  padding: EdgeInsets.all(24),
-  children: <Widget>[
-  Align(
-  alignment: Alignment.center,
-  child: Container(
-  width: imageWidth,
-  height: (imageWidth * 3) / 4,
-  child:Stack(
-  children: <Widget>[
-  Center(child: CircularProgressIndicator(
-  valueColor: new AlwaysStoppedAnimation<Color>(Theme.of(context).indicatorColor)
-  )),
-  Image.network(
-  widget.shot.images.two_x,
-  fit: BoxFit.fill,
-  )
-  ],
-  ),
-  ),
-  ),
-  Align(
-  alignment: Alignment.centerLeft,
-  child: Column(
-  mainAxisAlignment: MainAxisAlignment.center,
-  crossAxisAlignment: CrossAxisAlignment.center,
-  children: <Widget>[
-  Align(
-  alignment: Alignment.centerLeft,
-  child: Padding(
-  padding: EdgeInsets.fromLTRB(0, 24, 0, 8),
-  child: Text(
-  widget.shot.title,
-  style: TextStyle(
-  fontSize: 20,
-  color: Theme.of(context).indicatorColor),
-  textAlign: TextAlign.left,
-  softWrap: false,
-  ),
-  ),
-  ),
-  Align(
-  alignment: Alignment.centerLeft,
-  child: Text(
-  removeAllHtmlTags(widget.shot.description),
-  style: TextStyle(
-  fontSize: 14,
-  color: Theme.of(context).indicatorColor),
-  ),
-  )
-  ],
-  ),
-  )
-  ],
-  ),
-  ),
-  )*/
 }
